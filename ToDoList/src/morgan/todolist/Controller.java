68e0cb57b3081278b6c5ae9d50d9bbb6948d87af
Morgan Moon <morgonstivich@gmail.com>
2015-08-18 15:44:04 -0500

    Added create list functionality

--- ToDoList/src/morgan/todolist/Controller.java
+++ ToDoList/src/morgan/todolist/Controller.java
@@ -4,7 +4,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.ListView;
 
 public class Controller {
-    public ListView<ToDoList> toDoListListView;
+    public static ListView<ToDoList> toDoListListView;
     public TextField inputing;
 
     public void testing() throws Exception {
--- ToDoList/src/morgan/todolist/CreateListPopupController.java
+++ ToDoList/src/morgan/todolist/CreateListPopupController.java
@@ -4,11 +4,13 @@ package morgan.todolist;
  * Created by Morgan on 8/18/2015.
  */
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class CreateListPopupController {
 
     public Button cancelbtn;
+    public TextField listNameInput;
 
     public void test()
     {
@@ -18,6 +20,19 @@ public class CreateListPopupController {
     public void closeWindow(){
         Stage stage = (Stage)cancelbtn.getScene().getWindow();
         stage.close();
+    }
+
+    public void addList(){
+        if(listNameInput.getText().equals("") || listNameInput.getText().equals(null) ) {
+            ToDoList thelist2 = new ToDoList();
+            Controller.toDoListListView.getItems().add(thelist2);
+
+        }
+        else{
+            ToDoList thelist = new ToDoList(listNameInput.getText());
+            Controller.toDoListListView.getItems().add(thelist);
+        }
+        closeWindow();
     }
 
 }
